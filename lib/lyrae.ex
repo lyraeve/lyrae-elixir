f6a1a97bf78a9089970fb8c4305b283a73450de4
MilesChou <jangconan@gmail.com>
2019-03-31 02:33:04 +0800

    使用 httpotion 打 request

--- lib/lyrae.ex
+++ lib/lyrae.ex
@@ -1,5 +1,23 @@
 defmodule Lyrae do
   def main(args \\ []) do
-    IO.puts("hello world")
+    [head | _] = args
+    Lyrae.Finder.find_by_number(head, :javbus)
+  end
+end
+
+defmodule Lyrae.Finder do
+  def find_by_number(number, :javbus) do
+    Lyrae.Finder.Javbus.find_by_number(number)
+  end
+end
+
+defmodule Lyrae.Finder.Javbus do
+  def find_by_number(number) do
+    HTTPoison.get("https://www.javbus.com/" <> number) |> parse()
+  end
+
+  def parse {:error, _} do IO.puts "ERROR" end
+  def parse {:ok, response} do
+    response.body
   end
 end
